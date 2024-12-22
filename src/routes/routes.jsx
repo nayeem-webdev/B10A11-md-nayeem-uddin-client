99d713a5ffa7c4181dab9a07975b05388bb5edff
Nayeem Uddin <unayeem33@gmail.com>
2024-12-22 22:19:32 +0600

    Add food page added

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import ErrorPage from "../pages/ErrorPage";
+import AddFoodPage from "../components/AddFoodPage";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    errorElement: <ErrorPage/>
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/add-food",
+        element: <AddFoodPage />,
+      },
+    ],
   },
 ]);
 
